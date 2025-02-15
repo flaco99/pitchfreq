c7346310a72949fe24cb003e50b8d1a4a4b9bb29
Naomi Weissberg <nweissbe@uwaterloo.ca>
2025-02-14 20:56:52 -0500

    fixed air density. still linear

--- temp.py
+++ temp.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import io
 
 # please don't be weird and make sure you export with normal units:
@@ -135,7 +136,18 @@ def get_required_columns(lines):
     return df
 
 def get_air_density(altitude):
-    return altitude*(-0.00012333333)+1.2; # todo: make more accurate
+    # todo: make more accurate
+    # Returns air density (kg/m³) for a given altitude (m)
+    # Data from the table (Altitude in meters and corresponding Air Density in kg/m³)
+    ALTITUDES = np.array([0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000,
+                          15000, 20000, 25000, 30000, 40000, 50000, 60000, 70000, 80000])
+    DENSITIES = np.array([1.225, 1.112, 1.007, 0.9093, 0.8194, 0.7364, 0.6601, 0.5900,
+                          0.5258, 0.4671, 0.4135, 0.1948, 0.08891, 0.04008, 0.01841,
+                          0.003996, 0.001027, 0.0003097, 0.00008283, 0.00001846])
+
+    if altitude > ALTITUDES[-1]:
+        return 0.00001
+    return np.interp(altitude, ALTITUDES, DENSITIES)
 
 def add_air_and_inertia(df):
     if 'Altitude (m)' not in df.columns:
